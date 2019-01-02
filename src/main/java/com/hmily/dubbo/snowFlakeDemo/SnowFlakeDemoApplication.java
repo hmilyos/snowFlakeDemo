e5b2c242883848c5f22a68dcbf9bf9553a45a0fa
欧世明（深圳联友科技有限公司IS事业部项目一部制造领域) <ly-oushim@dfl.com.cn>
2019-01-02 09:32:42 +0800

    init

---
+++ src/main/java/com/hmily/dubbo/snowFlakeDemo/SnowFlakeDemoApplication.java
@@ -0,0 +1,14 @@
+package com.hmily.dubbo.snowFlakeDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowFlakeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowFlakeDemoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/hmily/dubbo/snowFlakeDemo/SnowFlakeDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hmily.dubbo.snowFlakeDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SnowFlakeDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
