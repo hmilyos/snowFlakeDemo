ea08dabe219acbdac5b1e7fba44a0ece3736aaea
欧世明（深圳联友科技有限公司IS事业部项目一部制造领域) <ly-oushim@dfl.com.cn>
2019-01-03 18:38:06 +0800

    dubbo

--- src/main/java/com/hmily/dubbo/snowFlakeDemo/service/impl/SnowFlakeServiceImpl.java
+++ src/main/java/com/hmily/dubbo/snowFlakeDemo/service/impl/SnowFlakeServiceImpl.java
@@ -5,7 +5,9 @@ import com.hmily.dubbo.snowFlakeDemo.service.ISnowFlakeService;
 import com.hmily.dubbo.snowFlakeDemo.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SnowFlakeServiceImpl implements ISnowFlakeService {
 
     private final  static Logger log = LoggerFactory.getLogger(SnowFlakeServiceImpl.class);
